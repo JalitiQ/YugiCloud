7816b68e077a2dbc15340dc33f91d78f0abf60d8
JalitiQ <contact@jullienl.com>
2025-08-10 12:19:17 +0200

    Mise en place de la page Web

--- YugiCloud/YugiLog/views.py
+++ YugiCloud/YugiLog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def login_user(request):
@@ -13,6 +12,17 @@ def login_user(request):
 			login(request, user)
 			return redirect("YugiWeb:index")
 		else:
-			return render(request, 'YugiLog:login')
-			
-	
+			return render(request, 'YugiLog:login', {'error_message': 'Nom d\'utilisateur incorrect'})
+		
+	else:
+		return render(request, 'YugiLog:login', {'error_message': 'Method Error'})
+
+def logout_user(request):
+	logout(request)
+	return redirect('YugiWeb:index')
+
+def register_user(request):
+	if request.method == 'POST':
+		username = request.method.POST.get('username')
+		password = request.method.POST.get('password')
+		
